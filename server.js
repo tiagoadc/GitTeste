3cf5480a2ba2cf70505f5893aae6cb480e924439
Tiago Araújo <49619223+tiagoadc@users.noreply.github.com>
2020-02-07 13:00:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
      res.status(200).send({message: 'Olá Mundo estou funcionando'});
 })
+app.get('/ola', (req, res) => {
+     res.status(200).send({message: 'Eu sou a rota ola'});
+})
+
 app.listen(3001, ()=> {
     console.log('API rodando na porta 3001');
 
